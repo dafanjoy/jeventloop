20fb4eaa0277690e6ccc60456a94cf690ec6b082
dafanjoy <danjoy@126.com>
2020-06-27 16:27:36 +0800

    基于事件的分发机制

--- src/main/java/com/fan/eventLoop/example/App.java
+++ src/main/java/com/fan/eventLoop/example/App.java
@@ -5,17 +5,13 @@ import com.fan.eventLoop.event.LoopEventListener;
 import com.fan.eventLoop.event.LoopEventSource;
 
 /**
- * Hello world!
+ * 基于事件机制触发消息分发
  *
  */
 public class App {
 	public static void main(String[] args) {
-		
 		LoopEventSource source = new LoopEventSource();
-		source.addEventListener(new LoopEventListener());
-		
-		
-		 
+		source.addEventListener(new LoopEventListener()); 
 		for (int i = 0; i < 1000000; i++) {
 			LoopEvent event = new LoopEvent(i);
 			source.notifies(event);
